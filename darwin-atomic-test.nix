f6fae5aaa14e6a1c9d7a5504daefda7c3593ca54
Bobby Rong <rjl931189261@126.com>
2022-03-27 17:09:26 +0800

    Create darwin-atomic-test.nix

---
+++ darwin-atomic-test.nix
@@ -0,0 +1,26 @@
+let
+  pkgs = import <nixpkgs> { };
+
+  testSrc = pkgs.writeText "test.c" ''
+    #include <glib.h>
+
+    int main() {
+        g_warning_once("attribute 'clip-path' given as CSS");
+
+        return 0;
+    }
+  '';
+in pkgs.runCommand "glib-collate-test" {
+    buildInputs = with pkgs; [
+      pkg-config
+      glib
+      clang
+    ];
+  } ''
+    mkdir -p $out
+
+    clang \
+      $(pkg-config --cflags glib-2.0 gmodule-2.0) \
+      $(pkg-config --libs glib-2.0 gmodule-2.0) \
+      "${testSrc}" -o "$out/test"
+  ''
